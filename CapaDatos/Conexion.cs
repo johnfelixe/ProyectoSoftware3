5e274a86b1747a61341c4e5bc61ae7214d0b53e9
johnfelixe <johnfelixe@gmail.com>
2022-11-05 20:23:15 -0500

    Modificacion color background

--- CapaDatos/Conexion.cs
+++ CapaDatos/Conexion.cs
@@ -8,6 +8,7 @@ namespace CapaDatos
 {
     public class Conexion
     {
-        public static string CN = "Data Source=.;Initial Catalog=DBPRUEBAS;Integrated Security=True";
+        //public static string CN = "Data Source=.;Initial Catalog=DBPRUEBAS;Integrated Security=True";
+        public static string CN = "Data Source=DBSistemaVentas.mssql.somee.com;Initial Catalog=DBSistemaVentas;user id=juanpgil_SQLLogin_1;pwd=vg2mt52z62";
     }
 }
